b5a2a712b744b092bd54f63f494e6b2f6ec0a555
HSKYOO <hyunseok888@gmail.com>
2026-07-02 12:37:38 +0900

    title music effect added

--- Assets/scripts/TitleManager.cs
+++ Assets/scripts/TitleManager.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class TitleManager : MonoBehaviour
 {
     public Image panel;
+    
+    // 추가: 오디오 소스를 연결할 변수
+    [Header("사운드 설정")]
+    public AudioSource startSound; 
 
     IEnumerator Fadeout()
     {
@@ -15,7 +19,6 @@ public class TitleManager : MonoBehaviour
         {
             color.a -= Time.deltaTime;
             panel.color = color;
-
             yield return null;
         }
     }
@@ -28,15 +31,24 @@ public class TitleManager : MonoBehaviour
         {
             color.a += Time.deltaTime;
             panel.color = color;
-
             yield return null;
         }
     }
 
     IEnumerator StartGameCoroutine()
     {
+        // 추가: 코루틴이 시작될 때(버튼을 누른 직후) 소리를 재생합니다.
+        if (startSound != null)
+        {
+            startSound.Play();
+        }
+
         panel.gameObject.SetActive(true);
+        
+        // 페이드인 연출(약 1초)이 끝날 때까지 대기
         yield return StartCoroutine(Fadein());
+        
+        // 페이드인이 끝나면 씬 전환
         SceneManager.LoadScene("Stage1");
     }
 
